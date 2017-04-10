58a8375aa07605139cc26e736b595d0cd1da5956
hongdago <hongdago@yahoo.com>
2017-04-10 15:37:46 +0800

    试探javascript

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -19,7 +19,7 @@ class ItemValidationTest(FunctionalTest):
 
         #首页刷新了，显示一个错误消息
         #提示代办事项不能为空
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error=self.get_error_element()
         self.assertEqual(error.text,'You can\'t have an empty list item')
 
         #她输入一些文字，然后再次提交，这次没有问题
@@ -34,7 +34,7 @@ class ItemValidationTest(FunctionalTest):
         
         #在列表页面她看到一个类似的错误
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error=self.get_error_element()
         self.assertEqual(error.text,'You can\'t have an empty list item')
         
         #输入文字之后就没问题了
@@ -56,5 +56,21 @@ class ItemValidationTest(FunctionalTest):
         #她看到一条有帮助的错误信息
         self.check_for_row_in_list_table('1: Buy wellies')
         time.sleep(2)
-        error=self.browser.find_element_by_css_selector('.has-error')
+        error=self.get_error_element()
         self.assertEqual(error.text,"You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        #伊迪丝新建一个清单，所有出现了一个验证错误
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(2)
+        error=self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        #为了消除错误，他开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a')
+        #看到错误消息消失了，她很高兴
+        error=self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+
